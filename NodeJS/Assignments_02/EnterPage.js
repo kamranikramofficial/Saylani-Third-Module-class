ed1654fb48e242cfd5dd03efe1694fda6009cfb5
kamranikramofficial <kamranikram706@gmail.com>
2025-08-04 05:18:03 -0700

    Convert previous assignment into folder module

---
+++ NodeJS/Assignments_02/EnterPage.js
@@ -0,0 +1,21 @@
+let  { AdditionMassage, AdditionFunction, SubtractionMassage, SubtractionFunction, MultiplicationMassage, MultiplicationFunction, DivisonMassage, DivisonFunction } = require("./Modules");
+
+
+
+console.log(AdditionMassage);
+AdditionFunction(5,10);
+ 
+console.log(SubtractionMassage);
+SubtractionFunction(10,5);
+
+console.log(MultiplicationMassage);
+
+MultiplicationFunction(5,5);
+
+
+console.log(DivisonMassage);
+
+DivisonFunction(5,5);
+
+
+console.log("All operations completed successfully.");
